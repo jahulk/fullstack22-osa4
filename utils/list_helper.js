7194741c8462ded181e371edabaa7b394ce40906
Jarkko Hulkko <jarkko.hulkko@student.jamk.fi>
2022-03-15 11:57:07 +0200

    apufunktioita ja yksikkötestejä, step5

---
+++ utils/list_helper.js
@@ -0,0 +1,58 @@
+const { urlencoded } = require('express')
+const _ = require('lodash')
+
+const dummy = (blogs) => {
+   return 1
+}
+
+const totalLikes = (blogs) => {
+   return blogs.reduce((acc, curr) => acc + curr.likes, 0)
+}
+
+const favoriteBlog = (blogs) => {
+   if (!blogs.length) {
+      return null
+   }
+
+   return _.maxBy(blogs, b => b.likes)
+}
+
+const mostBlogs = (blogs) => {
+   if (!blogs.length) {
+      return null
+   }
+
+   let blogsPerAuthor = []
+   _.forEach(_.countBy(blogs, b => b.author), (value, key) => {
+      blogsPerAuthor.push({
+         author: key,
+         blogs: value
+      })
+   })
+   
+   return _.maxBy(blogsPerAuthor, e => e.blogs)
+}
+
+const mostLikes = (blogs) => {
+   if (!blogs.length) {
+      return null
+   }
+
+   let likesPerAuthor = []
+   _.forEach(_.groupBy(blogs, b => b.author), (value, key) => {
+      likesPerAuthor.push({
+         author: key,
+         likes: _.sumBy(value, b => b.likes)
+      })
+   })
+
+   return _.maxBy(likesPerAuthor, e => e.likes)
+}
+
+module.exports = {
+   dummy,
+   totalLikes,
+   favoriteBlog,
+   mostBlogs,
+   mostLikes
+}
