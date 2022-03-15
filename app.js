1ad0d268f94f707846dcb26e4ba6940d7334612f
Jarkko Hulkko <jarkko.hulkko@student.jamk.fi>
2022-03-15 11:06:08 +0200

    blogilista, step2

---
+++ app.js
@@ -0,0 +1,32 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+
+mongoose
+   .connect(config.MONGODB_URI)
+   .then(() => {
+      console.log('connected to MongoDB')
+   })
+   .catch((error) => {
+      console.log('error connectin to MongoDB:', error.message)
+   })
+
+app.use(cors())
+app.use(express.json())
+
+morgan.token('body', (req) => {
+   return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
